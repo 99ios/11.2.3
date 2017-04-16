c9e3c51743f801f1e0a0b4da203be622c01b7876
test <422538722@qq.com>
2017-04-16 16:06:45 +0800

    11.2.3
    线程间通信

---
+++ 11.2.3 线程间通信/11.2.3 线程间通信/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  11.2.3 线程间通信
+//
+//  Created by 李维佳 on 2017/4/4.
+//  Copyright © 2017年 Liz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 11.2.3 线程间通信/11.2.3 线程间通信/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  11.2.3 线程间通信
+//
+//  Created by 李维佳 on 2017/4/4.
+//  Copyright © 2017年 Liz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
